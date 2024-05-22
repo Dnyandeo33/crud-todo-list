12c5f6dcf85a2a5fd2f1eb788f046c9a2a6a613a
Dnyandeo Waghunde <74929724+Dnyandeo33@users.noreply.github.com>
2024-05-22 09:53:14 +0200

    Update addToDoItem.js

--- api/addToDoItem.js
+++ api/addToDoItem.js
@@ -2,10 +2,8 @@ const addToDoItem = async (item) => {
     try {
         const res = await fetch('http://localhost:3000/todos', {
             method: 'POST',
-            mode: 'no-cors',
             headers: {
                 'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
 
             },
             body: JSON.stringify(item),
