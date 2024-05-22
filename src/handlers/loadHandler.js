daa4b273febacf26d263b9a1088c434cd3879ceb
Dnyandeo Waghunde <74929724+Dnyandeo33@users.noreply.github.com>
2024-05-22 09:52:16 +0200

    Update loadHandler.js

--- src/handlers/loadHandler.js
+++ src/handlers/loadHandler.js
@@ -3,13 +3,8 @@ import createToDoList from '../components/createToDoList.js';
 import dom from '../dom.js';
 
 const loadHandler = async () => {
-<<<<<<< HEAD
     const todos = await getItems();
     todos.forEach((todo) => {
-=======
-    const todo = await getItems();
-    todo.todos.forEach((todo) => {
->>>>>>> 4659ea86679f0ccedce3286fe0eeeb948c63fdb8
         const todoList = createToDoList(todo);
         dom.itemsList.append(todoList);
     });
